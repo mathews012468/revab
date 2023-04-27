a500f28115d6ac22a085ffe0f188f1213116b66c
mathews012468 <Mathew.Soto78@myhunter.cuny.edu>
2023-04-27 11:04:07 -0400

    update all views in game with actual info

--- revab/revabapp/views.py
+++ revab/revabapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from revabapp.src.revab import generate_abbrev
 
 # Create your views here.
 
@@ -8,5 +9,19 @@ def index(request):
     return render(request, "revabapp/index.html", context)
 
 def game(request):
-    context = {}
+    #add input screening
+    rounds = int(request.POST.get("rounds", 5))
+    abbrev_length = int(request.POST.get("abbrev_length", 3))
+    abbrev = generate_abbrev(abbrev_length)
+    attempts_per_round = int(request.POST.get("attempts_per_round", 3))
+
+    context = {
+        "rounds": rounds,
+        "abbrev": abbrev,
+        "abbrev_number": 1, 
+        "attempts_per_round": attempts_per_round,
+        "total_points": 0,
+        "round_history": [{"number": i+1, "abbrev": "...", "best_guess": "...", "score": "..."} for i in range(rounds)],
+        "guess_history": [{"number": i+1, "guess": "...", "result": "...", "score": "..."} for i in range(attempts_per_round - 1)]
+    }
     return render(request, "revabapp/game.html", context)
