536b9e8f997ace0558d4ec300e2289cc1d13f36d
mathews012468 <Mathew.Soto78@myhunter.cuny.edu>
2023-05-30 20:50:39 -0400

    preserve information passed to stats page

--- revab/revabapp/views.py
+++ revab/revabapp/views.py
@@ -34,7 +34,17 @@ def settings(request):
     return render(request, "revabapp/settings.html", context)
 
 def stats(request):
-    context = reasonable_defaults(request)
+    context = {
+        "rounds": request.POST.get('rounds'),
+        "attempts_per_round": request.POST.get('attempts_per_round'),
+        "abbrev_length": request.POST.get('abbrev_length'),
+        "abbrev": request.POST.get('abbrev'),
+        "guess_history": request.POST.get('guess_history'),
+        "round_history": request.POST.get('round_history'),
+        "total_points": request.POST.get('total_points'),
+        "settings": request.POST.get('settings'),
+        "destination": request.POST.get('destination')
+    }
     return render(request, "revabapp/stats.html", context)
 
 def help_page(request):
